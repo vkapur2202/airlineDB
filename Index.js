6f0da975e0eaf105a4d8fbd17112115b857936db
Alexander Harding Bradley <Alexanderhb02@gmail.com>
2021-05-05 19:40:07 -0400

    ach159: successfully created middleware for SQL query for adding customer

--- Index.js
+++ Index.js
@@ -1,13 +1,20 @@
+const { Pool} = require('pg')
+require('dotenv').config();
+const connectionString = process.env.CONNECTION_STRING
+const pool = new Pool({
+    connectionString,
+})
+
 const bodyparser = require('body-parser')
 const express = require("express")
 const path = require('path')
 const app = express()
-
+app.use(express.static('public'));
 var PORT = process.env.port || 3000
 
 // View Engine Setup
 app.set("views", path.join(__dirname))
-app.set("view engine", "html")
+app.set("view engine", "ejs")
 
 // Body-parser middleware
 app.use(bodyparser.urlencoded({extended:false}))
@@ -21,12 +28,16 @@ app.get("/createflight", function(req, res){
     res.sendFile(path.join(__dirname, '/Pages/FlightCreate.html'));
 });
 
-app.get("/searchflight", function(req, res){
-    res.sendFile(path.join(__dirname, '/Pages/FlightSearch.html'));
+app.get("/searchflights", function(req, res){
+    res.render('DynamicFile/FlightSearch');
 });
 
 app.get("/addcustomer", function(req, res){
     res.sendFile(path.join(__dirname, '/Pages/CustomerCreate.html'));
+});
+
+app.get("/addcustomeronflight", function(req, res){
+    res.sendFile(path.join(__dirname, '/Pages/CustomerFlightRegistration.html'));
 });
 
 app.get("/addairport", function(req, res){
@@ -45,7 +56,27 @@ app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, '/Pages/FlightCreate.html'));
 });
 app.post('/', (req, res) => {
+    const { pid, arrivalGate, pilotid, startaid, destinationaid, departuregate } = req.body
 
+    pool.query('INSERT INTO flight (pid, pilotid, startaid, destinationaid, distance, departuregate, arrivalgate) VALUES ($1, $2, $3, $4, $5, $6)', [pid, arrivalGate, pilotid, startaid, destinationaid, departuregate], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`Flight added with ID: ${result.insertId}`)
+    })
+    console.log("Using Body-parser: ", req.body)
+})
+
+app.post('/addcustomer', (req, res) => {
+    const { cname, username, password } = req.body
+
+    pool.query('INSERT INTO passenger (cname, username, password) VALUES ($1, $2, $3) RETURNING cid', [cname, username, password], (error, results) => {
+        if (error) {
+            throw error
+        }
+        console.log(results)
+        res.status(201).send(`Customer added with ID: ${results.rows[0].cid}`)
+    })
     console.log("Using Body-parser: ", req.body)
 })
 
