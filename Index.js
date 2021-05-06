27db850c7caacbfa3e58f84ec67dcb1a4706c65f
Alexander Harding Bradley <Alexanderhb02@gmail.com>
2021-05-05 22:08:37 -0400

    ach159: added more middleware, added SQL queries

--- Index.js
+++ Index.js
@@ -29,13 +29,27 @@ app.get("/createflight", function(req, res){
 });
 
 app.get("/searchflights", function(req, res){
-    res.render('DynamicFile/FlightSearch');
+    res.sendFile(path.join(__dirname, '/Pages/FlightSearch.html'));
 });
 
 app.get("/addcustomer", function(req, res){
     res.sendFile(path.join(__dirname, '/Pages/CustomerCreate.html'));
 });
 
+app.post("/searchflights", function(req, res){
+    //const cid = parseInt(request.params.cid)
+
+    const { depAirport, arrAirport, depDate, arrDate} = req.body
+
+    pool.query('SELECT * FROM flight WHERE startaid = $1 AND destinationaid = $2', [depAirport, arrAirport], (error, results) => {
+        if (error) {
+            throw error
+        }
+        console.log(results)
+        //res.status(200).json(results.rows)
+        res.render('DynamicFile/FlightSearch', {data: results})
+    })
+});
 app.get("/addcustomeronflight", function(req, res){
     res.sendFile(path.join(__dirname, '/Pages/CustomerFlightRegistration.html'));
 });
@@ -48,17 +62,37 @@ app.get("/addpilot", function(req, res){
     res.sendFile(path.join(__dirname, '/Pages/PilotCreate.html'));
 });
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, '/Pages/FlightCreate.html'));
-});
+app.post('/addpilot', (req, res) => {
+    const { pname} = req.body
+
+    pool.query('INSERT INTO pilot (pname) VALUES ($1) RETURNING pilotid', [pname], (error, results) => {
+        if (error) {
+            throw error
+        }
+        console.log(results)
+        res.status(201).send(`Pilot added with ID: ${results.rows[0].pilotid}`)
+    })
+})
+
+app.post('/addairport', (req, res) => {
+    const { aname, city, numgates} = req.body
+
+    pool.query('INSERT INTO airport (aname, city, numgates) VALUES ($1, $2, $3) RETURNING aid', [aname, city, numgates], (error, results) => {
+        if (error) {
+            throw error
+        }
+        console.log(results)
+        res.status(201).send(`Airport added with ID: ${results.rows[0].aid}`)
+    })
+})
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, '/Pages/FlightCreate.html'));
+    res.sendFile(path.join(__dirname, '/Pages/AirlineMainPage.html'));
 });
-app.post('/', (req, res) => {
+app.post('/createflight', (req, res) => {
     const { pid, arrivalGate, pilotid, startaid, destinationaid, departuregate } = req.body
 
-    pool.query('INSERT INTO flight (pid, pilotid, startaid, destinationaid, distance, departuregate, arrivalgate) VALUES ($1, $2, $3, $4, $5, $6)', [pid, arrivalGate, pilotid, startaid, destinationaid, departuregate], (error, results) => {
+    pool.query('INSERT INTO flight (pid, pilotid, startaid, destinationaid, distance, departuregate, arrivalgate) VALUES ($1, $3, $4, $5, $7, $6, $2)', [pid, arrivalGate, pilotid, startaid, destinationaid, departuregate, 500], (error, results) => {
         if (error) {
             throw error
         }
@@ -75,9 +109,8 @@ app.post('/addcustomer', (req, res) => {
             throw error
         }
         console.log(results)
-        res.status(201).send(`Customer added with ID: ${results.rows[0].cid}`)
+        res.status(201).send(`Passenger added with ID: ${results.rows[0].cid}`)
     })
-    console.log("Using Body-parser: ", req.body)
 })
 
 app.listen(PORT, function(error){
